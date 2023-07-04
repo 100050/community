896b01fe934046321943f5b5a485aedb3d99b267
100050 <82698917+100050@users.noreply.github.com>
2023-07-04 21:59:25 +0900

    db write

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
+import { getFirestore, collection, getDocs, setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-lite.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA-o2Iv9YpWi5uzc__gJ3KdrKcbo-8IiOc",
   authDomain: "community-ff496.firebaseapp.com",
+  databaseURL: "https://community-ff496-default-rtdb.firebaseio.com",
   projectId: "community-ff496",
   storageBucket: "community-ff496.appspot.com",
   messagingSenderId: "144487039080",
@@ -17,24 +17,28 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// firebaseConfig 정보로 firebase 시작
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// firebase의 firestore 인스턴스를 변수에 저장
+const db = getFirestore(app);
 
-const express = require('express')
-const router = express.Router();
+export async function readFirestore(title) {
+  const querySnapshot = await getDocs(collection(db, "articles"));
+  let t;
+  let c;
+  querySnapshot.forEach((doc) => {
+    if (title == doc.data().title) {
+      t = doc.data().title;
+      c = doc.data().content;
+    }
+  });
+  document.querySelector('#title').innerHTML = t;
+  document.querySelector('#content').innerHTML = c;
+}
 
-// config.js에서 export한 모듈을 다음과 같이 import 시킬 수 있다. (같은 디렉토리 위치)
-const database = require('./config');
-
-// localhost:3000/firebase/save 호출
-router.get('/save', function(req, res){
-    database.ref('customer').set({name : "junseok"}, function(error) {
-        if(error)
-            console.error(error)
-        else
-            console.log("success save !!");
-    });
-    return res.json({firebase : true});
-});
-
-module.exports = router;
+export async function writeFirestore(article_title, article_content) {
+  await setDoc(doc(db, "articles", article_title), {
+    title: JSON.stringify(article_title),
+    content: JSON.stringify(article_content)
+  });
+}
--- main.js
+++ main.js
@@ -1,34 +1,64 @@
-function alter(title) {
-    console.log(title);
-    document.querySelector('main').innerHTML='<section class="mainArticle"><h2>'+ title +'</h2><br><article>내용</article></section>';
-}
+import { readFirestore, writeFirestore } from "./db.js";
+
+const loginBtn = document.getElementById('login_btn');
+
+loginBtn.addEventListener("click", login)
 
 function login() {
     const ID = document.getElementById("id").value;
     const PW = document.getElementById("pw").value;
     if (ID == "1234" && PW == "1234") {
-        document.querySelector('.user').innerHTML='<div>'+ ID +'님</div> <button onclick="logout();">로그아웃</button> <br> <button onclick="wa();">글쓰기</button> <button>활동내역</button>'
+        document.getElementById('user').innerHTML='<div>'+ ID +'님</div> <button id="logout_btn">로그아웃</button> <br> <button id="writing_btn">글쓰기</button> <button>활동내역</button>'
+        document.getElementById('logout_btn').addEventListener("click", logout);
+        document.getElementById('writing_btn').addEventListener("click", getWriting);
     }
     else {
         alert("비밀번호나 아이디가 올바르지 않습니다.");
     }
-    
 }
 
 function logout() {
     document.querySelector('.user').innerHTML= '<form action="" class="login"><div><input type="text" id="id" placeholder="아이디"> <br><input type="password" id="pw" placeholder="비밀번호"></div><button onclick="login()">로그인</button><br><a href="">회원가입</a></form>';
 }
 
-function wa() {
+const articles = document.querySelectorAll('.article');
+articles.forEach((art) => {
+    art.addEventListener("click", () => {
+        request("article.html");
+        readFirestore(art.id);
+    })
+});
+
+function request(file) {
     const rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "writing.html", false);
-    rawFile.onreadystatechange = function () {
-        if(rawFile.readyState === 4) {
-            if(rawFile.status === 200 || rawFile.status == 0) {
-                var allText = rawFile.responseText;
-                document.querySelector("main").innerHTML = allText;
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function () {
+            if(rawFile.readyState === 4) {
+                if(rawFile.status === 200 || rawFile.status == 0) {
+                    var allText = rawFile.responseText;
+                    document.querySelector("main").innerHTML = allText;
+                }
             }
         }
+        rawFile.send(null);
+}
+
+function getWriting() {
+    request("writing.html");
+    const writeBtn = document.getElementById('write_btn');
+    writeBtn.addEventListener("click", writeArticle);
+}
+
+function writeArticle() {
+    const writeTitle = document.getElementById('write_title').value;
+    const writeContent = document.getElementById('write_content').value;
+    if (writeTitle == null) {
+        alert("제목을 입력해주세요.");
+        return;
     }
-    rawFile.send(null);
+    if (writeContent == null) {
+        alert("내용을 입력해주세요.");
+        return;
+    }
+    writeFirestore(writeTitle, writeContent);
 }
